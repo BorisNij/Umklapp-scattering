70e48eccd91b68be53207ed2f5e7165b650591df
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-12-15 16:02:47 -0500

    feat: implement course dao

--- src/main/java/net/bnijik/schooldbcli/dao/CourseQueries.java
+++ src/main/java/net/bnijik/schooldbcli/dao/CourseQueries.java
@@ -9,63 +9,54 @@ import java.util.Objects;
 
 @Component
 @PropertySource(factory = YamlPropertySourceFactory.class, value = "classpath:queries.yml")
-public class CourseQueries {
-    @Value("${course.create-course}")
-    private String createCourse;
-    @Value("${course.find-by-id}")
-    private String findById;
-    @Value("${course.find-by-name}")
-    private String findByName;
-    @Value("${course.find-all}")
-    private String finaAll;
-    @Value("${course.delete-by-id}")
-    private String deleteById;
+public record CourseQueries(@Value("${course.create}") String create,
+                            @Value("${course.find-by-id}") String findById,
+                            @Value("${course.find-by-name}") String findByName,
+                            @Value("${course.find-all}") String finaAll, @Value("${course.update}") String update,
+                            @Value("${course.delete-by-id}") String deleteById) {
 
-    public String createCourse() {
-        return createCourse;
-    }
-
-    public String findById() {
-        return findById;
-    }
-
-    public String findByName() {
-        return findByName;
-    }
-
-    public String finaAll() {
-        return finaAll;
-    }
-
-    public String deleteById() {
-        return deleteById;
-    }
+    public static final String COURSE_ID_PARAM = "courseId";
+    public static final String COURSE_NAME_PARAM = "courseName";
+    public static final String COURSE_DESCRIPTION_PARAM = "courseDescription";
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (CourseQueries) obj;
-        return Objects.equals(this.createCourse, that.createCourse) &&
-                Objects.equals(this.findById, that.findById) &&
-                Objects.equals(this.findByName, that.findByName) &&
-                Objects.equals(this.finaAll, that.finaAll) &&
-                Objects.equals(this.deleteById, that.deleteById);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CourseQueries that)) return false;
+        return Objects.equals(create, that.create) &&
+                Objects.equals(findById, that.findById) &&
+                Objects.equals(findByName, that.findByName) &&
+                Objects.equals(finaAll, that.finaAll) &&
+                Objects.equals(update, that.update) &&
+                Objects.equals(deleteById, that.deleteById);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(createCourse, findById, findByName, finaAll, deleteById);
+        return Objects.hash(create, findById, findByName, finaAll, update, deleteById);
     }
 
     @Override
     public String toString() {
-        return "CourseQueries[" +
-                "createCourse=" + createCourse + ", " +
-                "findById=" + findById + ", " +
-                "findByName=" + findByName + ", " +
-                "finaAll=" + finaAll + ", " +
-                "deleteById=" + deleteById + ']';
+        return "CourseQueries{" +
+                "create='" +
+                create +
+                '\'' +
+                ", findById='" +
+                findById +
+                '\'' +
+                ", findByName='" +
+                findByName +
+                '\'' +
+                ", finaAll='" +
+                finaAll +
+                '\'' +
+                ", update='" +
+                update +
+                '\'' +
+                ", deleteById='" +
+                deleteById +
+                '\'' +
+                '}';
     }
-
 }
--- src/main/java/net/bnijik/schooldbcli/model/Course.java
+++ src/main/java/net/bnijik/schooldbcli/model/Course.java
@@ -1,18 +1,8 @@
 package net.bnijik.schooldbcli.model;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public record Course(Long courseId, String courseName, String courseDescription) {
-
-    public Map<String, Object> toMap() {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("courseId", this.courseId);
-        parameters.put("courseName", this.courseName);
-        parameters.put("courseDescription", this.courseDescription);
-        return parameters;
-    }
 
     @Override
     public boolean equals(Object o) {
