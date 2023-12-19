4c48180827c82ff55a0e99357947e7f346a6041c
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-12-18 20:17:02 -0500

    refactor: remove overriding of `equals`

--- src/main/java/net/bnijik/schooldbcli/model/Course.java
+++ src/main/java/net/bnijik/schooldbcli/model/Course.java
@@ -1,17 +1,6 @@
 package net.bnijik.schooldbcli.model;
 
-import java.util.Objects;
-
 public record Course(Long courseId, String courseName, String courseDescription) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Course course)) return false;
-        return Objects.equals(courseId, course.courseId) &&
-                Objects.equals(courseName, course.courseName) &&
-                Objects.equals(courseDescription, course.courseDescription);
-    }
 
     @Override
     public String toString() {
