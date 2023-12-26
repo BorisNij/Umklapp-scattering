df01ab519e9b4464528ccd109f7288865592b4e2
Boris Nijikovsky <borisnijikovsky@gmail.com>
2023-12-26 15:18:02 -0500

    feat: add more logging

--- src/main/java/net/bnijik/schooldbcli/dao/group/JdbcGroupDao.java
+++ src/main/java/net/bnijik/schooldbcli/dao/group/JdbcGroupDao.java
@@ -33,11 +33,13 @@ public class JdbcGroupDao implements GroupDao {
     @Override
     public long save(Group group) {
         try {
+            log.debug("Saving group {}", group.groupName());
             final Number id = insert.executeAndReturnKey(Map.of(GroupQueries.GROUP_NAME_COLUMN, group.groupName()));
             if (id.longValue() == 0) {
                 log.error("Saving group failed. Returned ID: {}", id);
                 return 0;
             }
+            log.info("Successfully saved new group with ID {}", id);
             return id.longValue();
         } catch (DataAccessException e) {
             log.error("Error saving {} group: {}", group.groupName(), e.getMessage(), e);
@@ -49,6 +51,7 @@ public class JdbcGroupDao implements GroupDao {
     public Optional<Group> findById(long id) {
         String sql = queries.findById();
         try {
+            log.debug("Finding group with ID {}", id);
             return template.queryForStream(sql, Map.of(GroupQueries.GROUP_ID_PARAM, id), rowMapper).findFirst();
         } catch (DataAccessException e) {
             log.error("Error finding group with ID {}: {}", id, e.getMessage(), e);
@@ -60,6 +63,7 @@ public class JdbcGroupDao implements GroupDao {
     public Stream<Group> findAll(Page page) {
         String sql = queries.finaAll();
         try {
+            log.debug("Finding all groups with limit {}, offset {}", page.getLimit(), page.getOffset());
             return template.queryForStream(sql,
                                            Map.of(Page.PAGE_LIMIT_PARAM,
                                                   page.getLimit(),
@@ -76,6 +80,7 @@ public class JdbcGroupDao implements GroupDao {
     public Optional<Group> findByName(String name) {
         String sql = queries.findByName();
         try {
+            log.debug("Finding group by name {}", name);
             return template.queryForStream(sql, Map.of(GroupQueries.GROUP_NAME_PARAM, name), rowMapper).findFirst();
         } catch (DataAccessException e) {
             log.error("Error finding group with name {}: {}", name, e.getMessage(), e);
@@ -87,6 +92,10 @@ public class JdbcGroupDao implements GroupDao {
     public Stream<Group> findAllByMaxStudentCount(int maxStudentCount, Page page) {
         String sql = queries.findAllByMaxStudCount();
         try {
+            log.debug("Finding all groups having at most {} students with limit {} and offset {}",
+                      maxStudentCount,
+                      page.getLimit(),
+                      page.getOffset());
             return template.queryForStream(sql,
                                            Map.of(GroupQueries.GROUP_STUD_COUNT_PARAM,
                                                   maxStudentCount,
@@ -105,15 +114,17 @@ public class JdbcGroupDao implements GroupDao {
     public boolean update(Group newGroup, long existingGroupId) {
         String sql = queries.update();
         try {
+            log.debug("Updating group having ID {} with new name {}", existingGroupId, newGroup.groupName());
             final int rowsAffected = template.update(sql,
                                                      Map.of(GroupQueries.GROUP_NAME_PARAM,
                                                             newGroup.groupName(),
                                                             GroupQueries.GROUP_ID_PARAM,
                                                             existingGroupId));
             if (rowsAffected == 0) {
-                log.error("Failed to update group with ID {}. No rows were affected.", existingGroupId);
+                log.warn("No rows were affected while attempting to update group ID {}", existingGroupId);
                 return false;
             }
+            log.info("Successfully updated group having ID {} with new name {}", existingGroupId, newGroup.groupName());
             return true;
         } catch (DataAccessException e) {
             log.error("Error updating group with ID {}: {}", existingGroupId, e.getMessage(), e);
@@ -125,11 +136,13 @@ public class JdbcGroupDao implements GroupDao {
     public boolean delete(long id) {
         String sql = queries.deleteById();
         try {
+            log.debug("Deleting group with ID {}", id);
             final int rowsAffected = template.update(sql, Map.of(GroupQueries.GROUP_ID_PARAM, id));
             if (rowsAffected == 0) {
-                log.error("Failed to delete group with ID {}. No rows were affected.", id);
+                log.warn("No rows were affected while deleting group with {}", id);
                 return false;
             }
+            log.info("Successfully delete group with ID {}", id);
             return true;
         } catch (DataAccessException e) {
             log.error("Error deleting group with ID {}: {}", id, e.getMessage(), e);
